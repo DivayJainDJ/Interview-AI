6e6e8815b12fa673ad7b48d4f0461716c4c7ff2e
Divay Jain <divayjain1208@gmail.com>
2026-08-22 17:23:47 +0530

    update firebase configuration

--- frontend/src/utils/firebase.js
+++ frontend/src/utils/firebase.js
@@ -2,12 +2,13 @@
 import { initializeApp } from "firebase/app";
 import {getAuth, GoogleAuthProvider} from "firebase/auth"
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "fresherai-5e0c7.firebaseapp.com",
-  projectId: "fresherai-5e0c7",
-  storageBucket: "fresherai-5e0c7.firebasestorage.app",
-  messagingSenderId: "1021832157665",
-  appId: "1:1021832157665:web:be32d0a512a94b4d159aa0"
+  apiKey: "AIzaSyCZeScb9jlShpsFHFsx4USpz3_dupG2Yrc",
+  authDomain: "interviewai-24786.firebaseapp.com",
+  projectId: "interviewai-24786",
+  storageBucket: "interviewai-24786.firebasestorage.app",
+  messagingSenderId: "889366009830",
+  appId: "1:889366009830:web:5bb42ffb48f94585f3a827",
+  measurementId: "G-LTL0WGQ5WT"
 };
 
 
