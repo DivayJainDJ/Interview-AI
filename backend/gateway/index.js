0a1206e73face8a9a78ecc1c159e2ef41c803cf1
Divay Jain <divayjain1208@gmail.com>
2026-08-23 01:12:35 +0530

    remove legacy auth proxy

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -4,7 +4,6 @@ dotenv.config()
 import cors from "cors"
 import morgan from "morgan"
 import cookieParser from "cookie-parser"
-import proxy from "express-http-proxy"
 import { getCurrentUser } from "./controllers/user.controller.js"
 import { isAuth } from "./middleware/isAuth.js"
 import { proxyWithHeaders } from "./utils/proxyWithHeaders.js"
@@ -31,7 +30,6 @@ app.get("/" , (req,res)=>{
     res.send("Hello from Gateway")
 })
 
-app.use("/api/auth", proxy(process.env.AUTH_SERVICE_URL))
 app.use("/api/resume" ,isAuth, proxyWithHeaders(process.env.RESUME_SERVICE_URL))
 app.use("/api/interview",isAuth ,proxyWithHeaders(process.env.INTERVIEW_SERVICE_URL))
 app.use("/api/roadmap",isAuth ,proxyWithHeaders(process.env.ROADMAP_SERVICE_URL))
