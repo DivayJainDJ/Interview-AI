9feabf17316dcc162c8b5a9faba6fedbb212c277
Divay Jain <divayjain1208@gmail.com>
2026-08-22 15:21:12 +0530

    Initial commit

---
+++ backend/services/resume/config/pdf.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+import { PDFParse } from "pdf-parse";
+
+const extractText = async (filePath) => {
+    const buffer = fs.readFileSync(filePath);
+    const parser = new PDFParse({ data: buffer });
+
+    try {
+        const result = await parser.getText();
+        return result.text;
+    } finally {
+        await parser.destroy();
+    }
+};
+
+export default extractText;
+
+
+
