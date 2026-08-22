89d3c7124eafed4242df6d8701156dbca8176ee0
Divay Jain <divayjain1208@gmail.com>
2026-08-22 18:10:35 +0530

    fix firebase admin for render

--- backend/services/auth/configs/firebase.js
+++ backend/services/auth/configs/firebase.js
@@ -1,20 +1,10 @@
 import { initializeApp, cert } from "firebase-admin/app";
-import serviceAccountJson from "../serviceAccountKey.json" with { type: "json" };
 
-const {
-  FIREBASE_PROJECT_ID,
-  FIREBASE_CLIENT_EMAIL,
-  FIREBASE_PRIVATE_KEY,
-} = process.env;
-
-const serviceAccount =
-  FIREBASE_PROJECT_ID && FIREBASE_CLIENT_EMAIL && FIREBASE_PRIVATE_KEY
-    ? {
-        projectId: FIREBASE_PROJECT_ID,
-        clientEmail: FIREBASE_CLIENT_EMAIL,
-        privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      }
-    : serviceAccountJson;
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+};
 
 export const app = initializeApp({
   credential: cert(serviceAccount),
